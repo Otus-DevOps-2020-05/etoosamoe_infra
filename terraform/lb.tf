92f7da938584eef95ae268e77d2596b01e9ecb4c
Yuriy Semenkov <etoosamoe@yandex.ru>
2020-06-29 09:48:51 +0300

    hw#6 - dynamic target group

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -2,13 +2,16 @@ resource "yandex_lb_target_group" "reddit_lb_tg" {
   name      = "reddit-load-balancer-tg"
   region_id = "ru-central1"
 
-  target {
-    subnet_id = var.subnet_id
-    address   = yandex_compute_instance.app[0].network_interface.0.ip_address
-  }
-  target {
-    subnet_id = var.subnet_id
-    address   = yandex_compute_instance.app[1].network_interface.0.ip_address
+  dynamic "target" {
+    for_each = [for vm in yandex_compute_instance.app : {
+      subnet_id = var.subnet_id
+      address = vm.network_interface.0.ip_address
+    }]
+
+    content {
+    subnet_id = target.value.subnet_id
+    address   = target.value.address
+    }
   }
 }
 
