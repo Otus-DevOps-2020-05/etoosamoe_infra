1b00028e5143ca515f048998f000d8fc23e71d6f
Yuriy Semenkov <etoosamoe@yandex.ru>
2020-08-13 13:58:35 +0300

    terraform-2 hw, second try

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -3,9 +3,6 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
-  # key_id, secret
-  storage_access_key = var.storage_access_key
-  storage_secret_key = var.storage_secret_key
 }
 
 resource "yandex_storage_bucket" "create_bucket" {
